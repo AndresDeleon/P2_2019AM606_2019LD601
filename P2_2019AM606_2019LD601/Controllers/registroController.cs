c5fdfd9a6de94ab267b43484734eed2d50befe12
mery.acevedo <mery.acevedo@catolica.edu.sv>
2022-04-30 17:17:20 -0600

    cambios finales

--- P2_2019AM606_2019LD601/Controllers/registroController.cs
+++ P2_2019AM606_2019LD601/Controllers/registroController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using P2_2019AM606_2019LD601.Models;
 using System;
 using System.Collections.Generic;
@@ -17,10 +18,38 @@ namespace P2_2019AM606_2019LD601.Controllers
         this._contexto = context;
     }
 
-    
+
         public ActionResult Index()
         {
 
+            IEnumerable<Departamentos> datosDept = from d in _contexto.Departamentos
+                                                   select d;
+            List<SelectListItem> ComboDeptValores = new List<SelectListItem>();
+            foreach (Departamentos dept in datosDept)
+            {
+                SelectListItem miOpcion = new SelectListItem
+                {
+                    Text = dept.NOMBRE,
+                    Value = dept.ID.ToString()
+                };
+                ComboDeptValores.Add(miOpcion);
+            }
+
+            IEnumerable<Generos> datosGenero = from g in _contexto.Generos
+                                               select g;
+            List<SelectListItem> ComboGenValores = new List<SelectListItem>();
+            foreach (Generos gen in datosGenero)
+            {
+                SelectListItem miOpcion = new SelectListItem
+                {
+                    Text = gen.NOMBRE,
+                    Value = gen.ID.ToString()
+                };
+                ComboGenValores.Add(miOpcion);
+            }
+
+            ViewBag.ComboDeptValores = ComboDeptValores;
+            ViewBag.ComboGenValores = ComboGenValores;
 
             var lista = from m in _contexto.CasosReportados
                         join d in _contexto.Departamentos on m.ID_DEPARTAMENTOS equals d.ID
@@ -34,11 +63,68 @@ namespace P2_2019AM606_2019LD601.Controllers
                             FALLECIDOS = m.FALLECIDOS,
                             RECUPERADOS = m.RECUPERADOS
                         };
- 
-            IEnumerable <CasosReportados> ie = from c in _contexto.CasosReportados select c;
+
+            IEnumerable<CasosReportados> ie = from c in _contexto.CasosReportados select c;
 
 
             return View(lista);
         }
+        public IActionResult guardarRegistro(string DepartamentoSeleccionado, string GeneroSeleccionado, string Conf, string Recup, string Falle)
+        {
+            CasosReportados nuevoRegistro = new CasosReportados();
+            nuevoRegistro.ID_DEPARTAMENTOS = Int32.Parse(DepartamentoSeleccionado);
+            nuevoRegistro.ID_GENEROS = Int32.Parse(GeneroSeleccionado);
+            nuevoRegistro.CONFIRMADOS = Int32.Parse(Conf);
+            nuevoRegistro.RECUPERADOS = Int32.Parse(Recup);
+            nuevoRegistro.FALLECIDOS = Int32.Parse(Falle);
+
+            _contexto.CasosReportados.Add(nuevoRegistro);
+            _contexto.SaveChanges();
+
+            IEnumerable<Departamentos> datosDept = from d in _contexto.Departamentos
+                                                   select d;
+            List<SelectListItem> ComboDeptValores = new List<SelectListItem>();
+            foreach (Departamentos dept in datosDept)
+            {
+                SelectListItem miOpcion = new SelectListItem
+                {
+                    Text = dept.NOMBRE,
+                    Value = dept.ID.ToString()
+                };
+                ComboDeptValores.Add(miOpcion);
+            }
+
+            IEnumerable<Generos> datosGenero = from g in _contexto.Generos
+                                               select g;
+            List<SelectListItem> ComboGenValores = new List<SelectListItem>();
+            foreach (Generos gen in datosGenero)
+            {
+                SelectListItem miOpcion = new SelectListItem
+                {
+                    Text = gen.NOMBRE,
+                    Value = gen.ID.ToString()
+                };
+                ComboGenValores.Add(miOpcion);
+            }
+
+            ViewBag.ComboDeptValores = ComboDeptValores;
+            ViewBag.ComboGenValores = ComboGenValores;
+
+            var lista = from m in _contexto.CasosReportados
+                        join d in _contexto.Departamentos on m.ID_DEPARTAMENTOS equals d.ID
+                        join g in _contexto.Generos on m.ID_GENEROS equals g.ID
+                        select new CasosReportados
+                        {
+                            ID_CASOS_REPORTADOS = m.ID_CASOS_REPORTADOS,
+                            CONFIRMADOS = m.CONFIRMADOS,
+                            NOMBRE = d.NOMBRE,
+                            NOMBRE_DEP = g.NOMBRE,
+                            FALLECIDOS = m.FALLECIDOS,
+                            RECUPERADOS = m.RECUPERADOS
+                        };
+            return View(lista);
+        }
+
+
     }
 }
