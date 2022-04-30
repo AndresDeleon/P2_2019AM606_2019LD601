cccc464db474c7f5e7391f346931d8fa5acee740
Andrés Enrique Lemus Deleón <andres.lemus@catolica.edu.sv>
2022-04-30 14:12:17 -0600

    Primera Carga

---
+++ P2_2019AM606_2019LD601/Controllers/registroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2_2019AM606_2019LD601.Controllers
+{
+    public class registroController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
